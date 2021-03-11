5cf579595ddbce4bf9fecaf4f1ad2c9d5b172f21
MyrtheG <60790598+MyrtheGwenRijpma@users.noreply.github.com>
2021-03-11 09:59:18 -0800

    Update DCM_1_pre part_1_mac.m
    cleaned script up

--- DCM_1_pre part_1_mac.m
+++ DCM_1_pre part_1_mac.m
@@ -19,116 +19,15 @@ DCDate = datestr(date_input,formatOut);
 % create subject variable
 subj = strcat(pidn3,'_',DCDate);
 
-%% Set parameters
+%----------------------------------------%
+
+% Set parameters
 TR = 2; 
 n_vol = 235;
 
-%% Initialise SPM
-%--------------------------------------------------------------------------
+% Initialising SPM (add path under /Volumes/macdata/groups/rankin/spm12_new)
 spm('Defaults','fMRI');
 spm_jobman('initcfg');
 
-
-%% Create DCM regressor
+% Create DCM regressor
 DCMregressor(subj, main_dir, glmpath, TR, n_vol);
-
-%% Start looping
-% for ii=1:size(subj,1)
-%     
-%     % Print subject
-%     disp(['creating regressor file for ', subj{ii}]);
-%     
-%     %--------------------------------%
-%     % create multiple regressor file %
-%     %--------------------------------%
-% 
-%     % Retrieve motion and regressor files
-%     motionfile = 'rsfmri/interfmri_TRCNnSFmDI/motion_corr/rp_avol_006.txt';
-%     regressorfile = '/rsfmri/processedfmri_TRCNnSFmDI/stats_FC_R_vAI_SN_sphere_roi/timeseries/seed_nuisance_regressors.txt';
-% 
-%     % Load motion and regressor files
-%     load(fullfile(main_dir,subj{ii},motionfile));
-%     load(fullfile(main_dir,subj{ii},regressorfile));
-%     covar=[rp_avol_006,seed_nuisance_regressors(:,2:3)];
-% 
-%     % Save covariates
-%     % eval(['save ',main_dir,subj{ii},'/covar.mat',' covar']);
-% 
-%     % DCT.  Creates discrete cosine set with frequencies ranging from the UL to the
-%     % LL (default UL = 0.1Hz, LL = 1/128hz). Inputs are:
-%     % dir = path for output SPM
-%     % n = number of scans
-%     % covar =  multiple regressor file
-%     %--------------------------------------------------------------------------
-% 
-%     dir_ind = fullfile(main_dir, subj{ii});
-%     
-%     % specify accordingly
-%     TR=2; 
-%     n_vol=175; 
-%     
-%     [n_cols,R] = spm_glm_rest_dct(dir_ind,TR,n_vol,covar); % no mat file given. output is a text file
-%     
-%     % To Do: Update directory
-%     save(fullfile(dir_ind,'glm_regr.mat'), 'R');
-% 
-%     % -----------------------------------------------------
-%     % Extra steps
-%     % load ([main_dir,subj{ii},'\glm_regr.mat']); 
-%     % mreg_full_dct=[mreg_full_dct];
-%     % save (strcat(main_dir,subj{ii},'\mreg_full_dct.txt'),'mreg_full_dct')
-%     % -----------------------------------------------------
-% 
-%     %eval(['!gunzip ', dir_ind, '/',subj{ii},'_rfMRI_2_TT_N27_NonLinear_Warping_smoothed_GMS_masked.nii.gz'])
-% 
-%     %-------------------------------------------%
-%     % GLM SPECIFICATION, ESTIMATION & INFERENCE %
-%     %-------------------------------------------%
-% 
-%     % factors = load(fullfile(dir_ind,'glm_regr.mat')); % NEED TO CHECK!!!
-%     factors = load(fullfile(dir_ind,'glm_regr.mat')); % adapted
-%     f = spm_select('FPList', fullfile(dir_ind,'rsfmri/interfmri_TRCNnSFmDI/images/'), 'swuavol_.*');
-%     % f = cellstr(f)
-%     
-%     % img = cellstr(strcat(f, ',1'));
-%     for jj=1:n_vol
-%     img{jj,1}=[f(jj,:),',',int2str(1)];
-%     end
-% 
-%     clear matlabbatch
-% 
-%     % OUTPUT DIRECTORY
-%     %--------------------------------------------------------------------------
-%     matlabbatch{1}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent = cellstr(dir_ind);
-%     matlabbatch{1}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'GLM';
-% 
-% 
-%     % MODEL SPECIFICATION
-%     %--------------------------------------------------------------------------
-%     matlabbatch{2}.spm.stats.fmri_spec.dir = cellstr(fullfile(dir_ind,'GLM'));
-%     matlabbatch{2}.spm.stats.fmri_spec.timing.units = 'secs';
-%     matlabbatch{2}.spm.stats.fmri_spec.timing.RT = 2;
-%     matlabbatch{2}.spm.stats.fmri_spec.sess.scans = img(:,1);
-%     matlabbatch{2}.spm.stats.fmri_spec.sess.hpf = 100;
-%     matlabbatch{2}.spm.stats.fmri_spec.sess.multi_reg = cellstr([main_dir, subj{ii},'/glm_regr.mat']);
-%     matlabbatch{1,2}.spm.stats.fmri_spec.bases  = struct('none',1);
-% 
-%     % MODEL ESTIMATION
-%     %--------------------------------------------------------------------------
-%     matlabbatch{3}.spm.stats.fmri_est.spmmat = cellstr(fullfile(dir_ind,'GLM','SPM.mat'));
-% 
-%     % INFERENCE
-%     %--------------------------------------------------------------------------
-%     matlabbatch{4}.spm.stats.con.spmmat = cellstr(fullfile(dir_ind,'GLM','SPM.mat'));
-%     matlabbatch{4}.spm.stats.con.consess{1}.fcon.name = 'Effects of Interest';
-%     matlabbatch{4}.spm.stats.con.consess{1}.fcon.weights = eye(n_cols);
-% 
-%     save(fullfile(subj_DCM_fold,'model_spec_inference.mat'), 'matlabbatch');
-%     spm_jobman('run',matlabbatch);
-%     
-%     disp(['creating regressor file for ', subj{ii}, ' complete!!']);
-%     
-%     clear matlabbatch
-%     clear matlabbatch SPM
-% 
-% end
