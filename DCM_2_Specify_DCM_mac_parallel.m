35f5048f7e8d128e98f69aa8af735cb0c5ec170d
MyrtheG <60790598+MyrtheGwenRijpma@users.noreply.github.com>
2021-03-11 10:33:58 -0800

    Update DCM_2_Specify_DCM_mac_parallel.m
    cleaned script

--- DCM_2_Specify_DCM_mac_parallel.m
+++ DCM_2_Specify_DCM_mac_parallel.m
@@ -1,38 +1,31 @@
-%cd 'R:\groups\rankin\rsfMRI_Library\';
-
-pathFolder='/shared/macdata/groups/rankin/rsfMRI_Library/';
+%% Set directories
 main_dir='/shared/macdata/groups/rankin/rsfMRI_Library/';
-%pathFolder='D:\Winson\UCSF\projects\DCM\scans\';
 
 %% Set up filenames
-DCMfilename = 'SN_bilateral.mat'; %change name depending on model
+DCMfilename = 'SN_bilateral.mat'; %DCM for all 10 SN nodes selected (bilaterally)
 DCM_estimateoutput = ['estim_output_',DCMfilename];
 DCM_estimate = ['estimate_',DCMfilename];
 DCM_filenames = {DCMfilename, DCM_estimateoutput, DCM_estimate};
 
-%% Get Subjects list
-
-% Opening File directory
+% Opening excel spreadsheet with PIDN, DCDate named 'Sample_NC_bvFTD_8mm_2020'
 [Filename,PathName] = uigetfile({'*.xlsx';'*.xls';'*.*'},'Select the PIDN file');
 MCINT = readtable(fullfile(PathName,Filename));
 
-% Creating PIDN_DCDATE
+% formatting PIDN
 pidn = num2str(MCINT.PIDN);
 pidn2 = cellstr(pidn);
 pidn3 = regexprep(pidn2, '\W', '');
 
-% changing format of the date
+% formatting DCDATE
 dcdate = MCINT.DCDate; %correct
 date_input = datetime(dcdate,'ConvertFrom','excel');
 formatOut = 'mmddyyyy';
 DCDate = datestr(date_input,formatOut);
 
-%PIDNs = MCINT.PIDN_DCDATE; % PIDN_DCDATE variable
+% create subject variable
 subj = strcat(pidn3,'_',DCDate);
 
-
 %% Retrieve ROIs
-% Opening File directory
 [Filename,PathName] = uigetfile({'*.xlsx';'*.xls';'*.*'},'Select the ROI file');
 ROItable = readtable(fullfile(PathName,Filename));
 
@@ -40,12 +33,11 @@ for i=1:height(ROItable)
     rois{i,1} = ['VOI_',ROItable.ROI_name{i},'_',num2str(ROItable.size(i)),'mm_1.mat'];
 end
 
+%----------------------------------------%
 
-%% SPM
-
-% Initialise SPM
-%--------------------------------------------------------------------------
+% Initialising SPM (add path under /Volumes/macdata/groups/rankin/spm12_new)
 spm('Defaults','fMRI');
 spm_jobman('initcfg');
 
+% Specifying the DCM
 sTable = specifyDCM(subj, main_dir, DCM_filenames, rois)
